98e3f72d4b8f341d0052718cc1a0567616d0fd27
Gustavo Barreiro <barreirogustavo05@gmail.com>
2021-04-25 15:45:05 -0300

    entrega clase 3

---
+++ clase 3/entities/player/Player.gd
@@ -0,0 +1,37 @@
+extends Sprite
+#var arm
+
+onready var arm = $Arm
+
+var speed = 200 #Pixeles
+
+
+#const projectile_scene: PackedScene = load('res://entities/player/PlayerProyectile.tscn')
+func initialize(projectile_container):
+	arm.container = projectile_container
+
+#func _ready():
+#	arm = $Mano
+
+func _physics_process(delta):
+	# Manera básica
+	#var direction:int = 0
+	#if Input.is_action_pressed("move_left"):
+	#	direction = -1
+	#elif Input.is_action_pressed("move_right"):
+	#	direction = 1
+	
+	#position.x += direction * speed * delta
+	
+	#var mouse_position:Vector2= get_global_mouse_position()
+	var mouse_position:Vector2= get_local_mouse_position()
+	arm.rotation = mouse_position.normalized().angle()
+	
+	if Input.is_action_just_pressed("fire"):
+		arm.fire()
+
+	# Manera optimizada
+	var direction_optimized:int = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	
+	position.x += direction_optimized * speed * delta
+
